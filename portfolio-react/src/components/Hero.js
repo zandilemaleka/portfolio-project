29d2d6931c0c500edb5fee5631bca1459114d932
zandilemaleka <malekabaatile@gmail.com>
2025-11-30 14:10:35 +0200

    Added a link to my github on <a> View my work </a>

--- portfolio-react/src/components/Hero.js
+++ portfolio-react/src/components/Hero.js
@@ -10,19 +10,24 @@ const Hero = () => {
         <h1 className="text-4xl md:text-5xl font-bold mb-6 text-gray-800 leading-tight">
           Welcome to My Portfolio
         </h1>
-      
-<div className="text-justify mb-8">
-        <p className="text-lg md:text-xl text-gray-600 max-w-xl mx-auto leading-relaxed">
-          I'm Zandile Maleka, a passionate developer. As a developer, I
-          specialize in building user-friendly applications with clean,
-          efficient code.Always eager to take on new challenges and learn
-          emerging technologies.
-        </p>
+
+        <div className="text-justify mb-8">
+          <p className="text-lg md:text-xl text-gray-600 max-w-xl mx-auto leading-relaxed">
+            I'm Zandile Maleka, a passionate developer. As a developer, I
+            specialize in building user-friendly applications with clean,
+            efficient code.Always eager to take on new challenges and learn
+            emerging technologies.
+          </p>
         </div>
         <div className="flex flex-col sm:flex-row justify-center gap-4">
-          <button className="bg-pink-500 text-white font-bold px-5 py-3 rounded-lg hover:bg-pink-700">
+          <a
+            href="https://github.com/zandilemaleka"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-pink-500 text-white font-bold px-5 py-3 rounded-lg hover:bg-pink-700 text-center"
+          >
             View My Work
-          </button>
+          </a>
           <a
             href="#contact"
             className="border border-pink-600 text-pink-600 px-6 py-3 rounded-lg hover:bg-pink-50"
